d57b6d56679223a2d7add93bfb9ec2bf2d4cc952
shanand <shanandshrestha@gmail.com>
2021-05-19 18:44:35 +0545

    added loop conditions on themeleaf view file

--- thymeleaf/src/main/java/com/example/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.thymeleaf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.thymeleaf.model.User;
 
 @Controller
 public class UserController {
@@ -14,6 +19,16 @@ public class UserController {
 		model.addAttribute("GPA", convertGPA(grade));
 		//return to templates/demo.html page
 		return "demo";
+	}
+	
+	@RequestMapping("demo2")
+	public String demo2(Model model) {
+		List<User> lst = new ArrayList<User>();
+		lst.add(new User(1, "Tom", 30));
+		lst.add(new User(2, "Jerry", 29));
+		lst.add(new User(3, "Nancy", 27));
+		model.addAttribute("list", lst);
+		return "demo2";
 	}
 	
 	private String convertGPA(double grade) {
