38e84ea3630f94243e33f98fb594c0b07768c368
shanand <shanandshrestha@gmail.com>
2021-05-19 15:31:00 +0545

    solved the issues with freemarker viewresolver servlet dispatcher

--- freemarker/src/main/java/com/example/freemarker/controller/StudentList.java
+++ freemarker/src/main/java/com/example/freemarker/controller/StudentList.java
@@ -6,20 +6,21 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.example.freemarker.model.Student;
 
 @Controller
 public class StudentList {
+	
 	@RequestMapping("/list")
-	@ResponseBody
-	public List<Student> list(Model model) {
+	public String list(Model model) {
 		List<Student> list = new ArrayList<>();
 		list.add(new Student(1, "Tom", 80.5));
 	    list.add(new Student(2, "Jerry", 90.4));
 	    list.add(new Student(3, "Paul", 77.5));
 	    model.addAttribute("list", list);
-	    return list;
+	    return "list";
 	}
 }
