797fecd6237f643c06365b66976f9d97b40bf8e4
shanand <shanandshrestha@gmail.com>
2021-05-19 15:14:57 +0545

    some changes to freemarker

--- freemarker/src/main/java/com/example/freemarker/controller/StudentList.java
+++ freemarker/src/main/java/com/example/freemarker/controller/StudentList.java
@@ -6,18 +6,20 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.freemarker.model.Student;
 
 @Controller
 public class StudentList {
 	@RequestMapping("/list")
-	public String list(Model model) {
+	@ResponseBody
+	public List<Student> list(Model model) {
 		List<Student> list = new ArrayList<>();
 		list.add(new Student(1, "Tom", 80.5));
 	    list.add(new Student(2, "Jerry", 90.4));
 	    list.add(new Student(3, "Paul", 77.5));
 	    model.addAttribute("list", list);
-	    return "list";
+	    return list;
 	}
 }
