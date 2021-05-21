0c1f750e99aca3db26da00fd46e62b4c1fa7db20
shanand <shanandshrestha@gmail.com>
2021-05-21 21:32:58 +0545

    added simple exception mapping resolver

---
+++ spring-boot-exception/src/main/java/com/example/springbootexception/exceptions/MySimpleMappingExceptionResolver.java
@@ -0,0 +1,20 @@
+package com.example.springbootexception.exceptions;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+@Configuration
+public class MySimpleMappingExceptionResolver {
+	@Bean
+	public SimpleMappingExceptionResolver getSimpleMappingExceptionResolver() {
+		SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
+		Properties mapping = new Properties();
+		mapping.put("java.lang.ArithmeticException", "mathError");
+		mapping.put("java.lang.NullPointerException", "nullPointerError");
+		resolver.setExceptionMappings(mapping);
+		return resolver;
+	}
+}
