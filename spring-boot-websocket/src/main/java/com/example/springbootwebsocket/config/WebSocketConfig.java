5255ddb0bcb0906bdd6c7f00dd90cde73be6ccc9
shanand <shanandshrestha@gmail.com>
2021-05-22 19:19:34 +0545

    modified and added code to view

--- spring-boot-websocket/src/main/java/com/example/springbootwebsocket/config/WebSocketConfig.java
+++ spring-boot-websocket/src/main/java/com/example/springbootwebsocket/config/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	 @Override
 	 public void registerStompEndpoints(StompEndpointRegistry registry) {
-		 registry.addEndpoint("websocket-demo").withSockJS();
+		 registry.addEndpoint("/chat");
+	        registry.addEndpoint("/chat").withSockJS();
 	 }
 }
